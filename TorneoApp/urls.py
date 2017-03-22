52009451bf9dabe51ca7a6f05eca3d90802efc5f
Adriann Sanchez <alka65@hotmail.com>
2017-03-22 11:33:33 -0500

    send email

--- TorneoApp/urls.py
+++ TorneoApp/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
 	url(r'^', include('TorneoApp.apps.inscripcion.urls')),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 	url(r'^admin/', admin.site.urls),
 ]
