53774a38585e5f92d4dac1e41450f60edc9b4919
Adriann Sanchez <alka65@hotmail.com>
2017-04-23 18:55:07 -0500

    export data

--- TorneoApp/apps/inscripcion/urls.py
+++ TorneoApp/apps/inscripcion/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
 	url(r'^$', InscripcionView.as_view(), name = 'inscripcion'),
+	url(r'^export/(?P<level>\w+)/$', export_data, name = 'export_data'),
 ]
--- TorneoApp/apps/inscripcion/views.py
+++ TorneoApp/apps/inscripcion/views.py
@@ -1,7 +1,9 @@
 # -*- encoding: utf-8 -*-
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 from .forms import *
 
 var_dir_template = 'inscripcion/'
@@ -16,3 +18,14 @@ class InscripcionView(SuccessMessageMixin, CreateView):
 		context = super(InscripcionView, self).get_context_data(**kwargs)
 		context['title'] = 'Torneo de ajedrez'
 		return context
+
+def export_data(request, level):
+	jugadores = Inscritos.objects.all().order_by('fullname')
+	jugadores = jugadores if level == 'all' else jugadores.filter(nivel = level)
+	content = 'No;NombreCompleto;Nombre;Apellido\n'
+	for jugador in jugadores:
+		jugador_split = jugador.fullname.split(' ')
+		content += str(jugador.pk)+';'+jugador.fullname.title()+';'+((jugador_split[0]+' '+jugador_split[1]).title() if len(jugador_split) == 4 else jugador_split[0].title())+';'+((jugador_split[2]+' '+jugador_split[3]).title() if len(jugador_split) == 4 else (jugador_split[1]+' '+jugador_split[2]).title())+';\n'
+	response = HttpResponse(content, content_type='text/plain')
+	response['Content-Disposition'] = 'attachment; filename=inscritos.txt'
+	return response
