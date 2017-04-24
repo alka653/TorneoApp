1170b0f0a874b05298163c0d0fedaf1de60a6fd8
Adriann Sanchez <alka65@hotmail.com>
2017-04-23 19:41:40 -0500

    export data

--- TorneoApp/apps/inscripcion/views.py
+++ TorneoApp/apps/inscripcion/views.py
@@ -25,7 +25,7 @@ def export_data(request, level):
 	content = 'No;NombreCompleto;Titulo;ID;Elonac;FIDE;FNac;Fed;Sex;Tipo;Gr;NoClub;NombreClub;FIDE Id;Fuente;Pts;Des1;Des2;Des3;Des4;Des5;Clas;Apellido;Nombre;titulo\n'
 	for jugador in jugadores:
 		jugador_split = jugador.fullname.split(' ')
-		content += str(jugador.pk)+';'+jugador.fullname.title().encode('utf-8')+';;;0;0;;AUT;;;;0;;0;;0;0;0;0,00;;;'+str(jugador.pk)+';'+((jugador_split[0]+' '+jugador_split[1]).title().encode('utf-8') if len(jugador_split) == 4 else jugador_split[0].title())+';'+((jugador_split[2]+' '+jugador_split[3]).title() if len(jugador_split) == 4 else (jugador_split[1]+' '+jugador_split[2]).title()).encode('utf-8')+';\n'
+		content += str(jugador.pk)+';'+jugador.fullname.title().decode('utf-8')+';;;0;0;;AUT;;;;0;;0;;0;0;0;0,00;;;'+str(jugador.pk)+';'+((jugador_split[0]+' '+jugador_split[1]).title().decode('utf-8') if len(jugador_split) == 4 else jugador_split[0].title())+';'+((jugador_split[2]+' '+jugador_split[3]).title() if len(jugador_split) == 4 else (jugador_split[1]+' '+jugador_split[2]).title()).decode('utf-8')+';\n'
 	response = HttpResponse(content, content_type='text/plain')
 	response['Content-Disposition'] = 'attachment; filename=inscritos.TXT'
 	return response
